e6dec964d42a744af6b8548f8ec8bd55b7d85546
Boy <sivertpaulsen2@gmail.com>
2022-07-20 17:52:16 +0200

    initial commit

--- chatty-paper/build.gradle.kts
+++ chatty-paper/build.gradle.kts
@@ -9,6 +9,8 @@ plugins {
 
 repositories {
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://nexus.scarsz.me/content/groups/public/") // DiscordSRV
+    maven("https://m2.dv8tion.net/releases") // DiscordSRV
 }
 
 dependencies {
@@ -25,6 +27,7 @@ dependencies {
 
     /// Third-party plugins
     compileOnly(chattyLibs.placeholderapi)
+    compileOnly(chattyLibs.discordsrv)
 
     // Shaded
     implementation(libs.idofront.core)
