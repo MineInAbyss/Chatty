0d89541476f265092f3f9a447ae13feb2b0a519f
Boy0000 <62521371+Boy0000@users.noreply.github.com>
2022-07-22 07:36:33 +0200

    implement book and chat formatting (#14)

--- chatty-paper/src/main/kotlin/com/mineinabyss/chatty/listeners/PlayerListener.kt
+++ chatty-paper/src/main/kotlin/com/mineinabyss/chatty/listeners/PlayerListener.kt
@@ -4,8 +4,12 @@ import com.mineinabyss.chatty.components.HideJoinLeave
 import com.mineinabyss.chatty.components.PlayerData
 import com.mineinabyss.chatty.helpers.*
 import com.mineinabyss.geary.papermc.access.toGeary
+import com.mineinabyss.idofront.messaging.miniMsg
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.block.SignChangeEvent
+import org.bukkit.event.player.PlayerEditBookEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 
@@ -38,5 +42,46 @@ class PlayerListener : Listener {
             quitMessage(translatePlaceholders(player, chattyMessages.joinLeave.leaveMessage))
 //        if (chattyConfig.leave.sendAcrossProxy)
 //            Bukkit.getServer().sendPluginMessage(chattyPlugin, chattyProxyChannel, PlaceholderAPI.setPlaceholders(player, messages.proxies.proxyLeave).toByteArray())
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    fun PlayerEditBookEvent.onBookSign() {
+        val meta = newBookMeta
+        val config = chattyConfig.book
+        if (player.hasPermission(chattyConfig.book.bypassFormatPermission)) {
+            if (meta.hasAuthor() && !config.useDisplayNameForAuthor)
+                meta.author(newBookMeta.author().fixLegacy())
+            else if (meta.hasAuthor() && config.useDisplayNameForAuthor)
+                meta.author(player.displayName())
+            if (meta.hasTitle())
+                meta.title(newBookMeta.title().fixLegacy())
+            if (meta.hasPages())
+                meta.pages(newBookMeta.pages().map { it.fixLegacy() })
+        }
+        else {
+            if (meta.hasAuthor() && !config.useDisplayNameForAuthor)
+                meta.author(newBookMeta.author().serialize().verifyBookStyling().miniMsg())
+            else if (meta.hasAuthor() && config.useDisplayNameForAuthor)
+                meta.author(player.displayName().serialize().verifyBookStyling().miniMsg())
+            if (meta.hasTitle())
+                meta.title(newBookMeta.title().serialize().verifyBookStyling().miniMsg())
+            if (meta.hasPages())
+                meta.pages(newBookMeta.pages().map { it.serialize().verifyBookStyling().miniMsg() })
+        }
+        newBookMeta = meta
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    fun SignChangeEvent.onSign() {
+        if (player.hasPermission(chattyConfig.sign.bypassFormatPermission)) {
+            lines().forEachIndexed { index, line ->
+                line(index, line.fixLegacy())
+            }
+        }
+        else {
+            lines().forEachIndexed { index, line ->
+                line(index, line.serialize().verifySignStyling().miniMsg())
+            }
+        }
     }
 }
